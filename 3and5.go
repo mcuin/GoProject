da85572e53690f2c0f0a2d4675be82af1be6d4bd
Mykal Cuin <mykcuin@gmail.com>
2014-09-24 16:30:13 -0600

    Working on the 3&5 problem proposed on Euler Project.

---
+++ 3and5.go
@@ -0,0 +1,21 @@
+//Sum of multiples of 3 & 5 from Project Euler
+
+package main
+
+import "fmt"
+
+func loop() {
+	var j int
+	for i := 3; i <= 1000; i++ {
+		if i % 3 == 0 || i % 5 == 0 {
+			j += i
+		} else {
+			return
+		}
+	}
+	fmt.Println(j)
+}
+
+func main() {
+	loop()
+}
